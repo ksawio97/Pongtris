8b2f80720704c15d49363dfd830db562c86c0138
ksawio97 <ksawery9708@gmail.com>
2023-02-07 16:50:27 +0100

    Added EffectsController

---
+++ Assets/Scripts/EffectsController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+
+namespace Assets.Scripts
+{
+    public static class EffectsController
+    {
+
+        private static Action[] effects = new Action[]
+        {
+        CreateBall,
+        Explode
+        };
+
+        public static void DoRandomEffect()
+        {
+            effects[UnityEngine.Random.Range(0, effects.Length)]();
+        }
+
+        private static void CreateBall()
+        {
+            Debug.Log("New Ball!");
+        }
+
+        private static void Explode()
+        {
+            Debug.Log("Explosion!");
+        }
+    }
+}
