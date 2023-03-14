8d0419fbdd56e4bbbad534f4a2328a48082d4b5d
ksawio97 <ksawery9708@gmail.com>
2023-03-14 15:49:59 +0100

    Fixed bugs occuring while spawning boxes

--- Assets/Scripts/BoxesManager.cs
+++ Assets/Scripts/BoxesManager.cs
@@ -20,9 +20,6 @@ public class BoxesManager : MonoBehaviour
     private Vector3 moveVec;
     private float moved;
 
-    private float defaultMoveVec;
-    private float maxMoveVec;
-
     Vector3[] boxSpawnPositions;
 
     void Start()
@@ -31,38 +28,25 @@ public class BoxesManager : MonoBehaviour
         float aspectRatio = (float)Screen.width / (float)Screen.height;
 
         camSize = new Vector2(cam.orthographicSize * 2 * aspectRatio, cam.orthographicSize * 2);
-        defaultMoveVec = camSize.x / 5 / 150;
 
         //if didn't load
         if (boxes == null)
         {
             boxes = new ();
             moveAmount = camSize.x / 5;
-            moveVec = new Vector2(0, defaultMoveVec);
+            moveVec = new Vector2(0, camSize.x / 5 / 150);
             moved = moveAmount;
         }
+
+
         boxSpawnPositions = new Vector3[5];
 
         for(int i = 0; i < boxSpawnPositions.Length; i++)
-            boxSpawnPositions[i] = new Vector3(camSize.x / 5 * i + camSize.x / 5 / 2 - camSize.x / 2, camSize.y / 2 + camSize.x / 5 / 2);
-
-        maxMoveVec = defaultMoveVec * 2;
-        //30s / 0.5s = 60
-        StartCoroutine("BoostSpeed", (maxMoveVec - defaultMoveVec) / 60);
-    }
-
-    System.Collections.IEnumerator BoostSpeed(float moveBoost)
-    {
-        yield return new WaitForSeconds(0.5f);
-        moveVec.y += moveBoost;
-        if (moveVec.y < maxMoveVec)
         {
-            StartCoroutine("BoostSpeed", moveBoost);
-
+            boxSpawnPositions[i] = new Vector3(camSize.x / 5 * i + camSize.x / 5 / 2 - camSize.x / 2, camSize.y / 2 + camSize.x / 5 / 2);
         }
-        else
-            Debug.Log($"Max Move Amount reached {moveVec.y}");
     }
+
     void FixedUpdate()
     {
         if (boxes.Count == 0)
